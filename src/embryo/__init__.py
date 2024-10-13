f032bbe99934152d94ac1f5c4076aafccb6f8eeb
denisfaer <74679045+denisfaer@users.noreply.github.com>
2024-10-13 14:06:25 -0400

    edit: changed class Cell in __init__.py to work with Point and Fate

--- src/embryo/__init__.py
+++ src/embryo/__init__.py
@@ -4,10 +4,8 @@ import dataclasses
 class Point: # holds the spatial coordinates of a point
     x: int
     y: int
-    
     def dist(self, dest):
         return ((self.x - dest.x) ** 2 + (self.y - dest.y) ** 2) ** 1/2
-
 
 @dataclasses.dataclass
 class Fate: # holds the ODE x, y (EPI-PE axis, Specification axis)
@@ -15,10 +13,9 @@ class Fate: # holds the ODE x, y (EPI-PE axis, Specification axis)
         self.x = x
         self.y = y
 
-    commit: bool # has the cell passed bifurcation
-    spec: bool # has the cell reached an attractor point
-
 @dataclasses.dataclass
-class Cell:
-    x: int
-    y: int
+class Cell: # object of a single cell specifying location and fate
+    def __init__(self, p: Point, f: Fate):
+        self.loc = p
+        self.fate = f
+        self.history = [[p, f]]
