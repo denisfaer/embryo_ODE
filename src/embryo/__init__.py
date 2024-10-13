c2b0356df441f9ed29d9c7a7f3d04b450f33debd
denisfaer <74679045+denisfaer@users.noreply.github.com>
2024-10-13 13:45:05 -0400

    edit: added class Point to __init__.py

--- src/embryo/__init__.py
+++ src/embryo/__init__.py
@@ -1,6 +1,14 @@
 import dataclasses
 
 @dataclasses.dataclass
+class Point: # holds the spatial coordinates of a point
+    x: int
+    y: int
+    
+    def dist(self, dest):
+        return ((self.x - dest.x) ** 2 + (self.y - dest.y) ** 2) ** 1/2
+
+@dataclasses.dataclass
 class Cell:
-    status: bool
-    age: int
+    x: int
+    y: int
