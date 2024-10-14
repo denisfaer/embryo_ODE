72a73c96d61fec05eccf0c7dede0c48d4def9d6e
DezrannCAS <120508422+DezrannCAS@users.noreply.github.com>
2024-10-13 21:53:18 -0400

    Small change on sqrt (power of 1/2)

--- src/epipeODE/__init__.py
+++ src/epipeODE/__init__.py
@@ -7,7 +7,7 @@ class Point: # holds the position of a point in a 3D sapce (unused dimensions ar
     y: int = 0
     z: int = 0
     def dist(self, dest):
-        return ((self.x - dest.x) ** 2 + (self.y - dest.y) ** 2 + (self.z - dest.z) ** 2) ** 1/2
+        return ((self.x - dest.x) ** 2 + (self.y - dest.y) ** 2 + (self.z - dest.z) ** 2) ** (1/2)
 
 @dataclasses.dataclass
 class Fate: # holds the ODE x, y (EPI-PE axis, Specification axis)
