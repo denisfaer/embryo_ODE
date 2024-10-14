e6044dddf6b0e0dae2d486c79a2d453121e7be4f
DezrannCAS <120508422+DezrannCAS@users.noreply.github.com>
2024-10-13 22:53:10 -0400

    Version with formulation of f2 (f1 still zero)

--- src/model/geometrical_models.py
+++ src/model/geometrical_models.py
@@ -1,45 +1,63 @@
-from embryo import Cell
+from epipeODE import Cell, Point, Fate
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
 
 @dataclass
-class GeometricalModel(ABC):
+class GeomModel(ABC): # general modeling class
     dt: float = 0.01
 
     @abstractmethod
-    def potential(self, cell: Cell):
+    def potential(self, cell: Fate):
         pass
 
     @abstractmethod
-    def gradient(self, cell: Cell):
+    def gradient(self, cell: Fate):
         pass
 
-    def update_state(self, cell: Cell):
-        dx, dy = self.gradient(cell)
-        cell.fate.x += dx * self.dt
-        cell.fate.y += dy * self.dt
-        cell.fate.apply_noise()
+    def update_state(self, cell: Cell): # evolve the simulation 1 time-step
+        dx, dy = self.gradient(cell.fate)
+	temp_x = cell.fate.x + dx * self.dt
+	temp_y = cell.fate.y + dy * self.dt
+	temp = Fate(temp_x, temp_y)
+        temp.apply_noise()
+	location = Point(0, 0) # in the future this will update cell movement
+	cell.history.append([location, temp])
+	cell.fate.x = temp.x
+	cell.fate.y = temp.y
+
 
 @dataclass
-class DualCuspModel(GeometricalModel):
+class DualCusp(GeomModel): # Dual Cusp model from Raju & Siggia (2024)
     K1: float = 0.15
+    f1: float = 0.0
 
-    def potential(self, cell: Cell):
-        return cell.fate.x**4 + cell.fate.y**4 - cell.fate.y**3 + 4 * cell.fate.x**2 * cell.fate.y + cell.fate.y**2 - self.K1 * cell.fate.y
+    def potential(self, cell: Fate):
+        return cell.x ** 4 + cell.y ** 4 - cell.y ** 3 + 4 * cell.x ** 2 * cell.y + cell.y ** 2 + self.f_1 * cell.x - self.K1 * cell.y
 
-    def gradient(self, cell: Cell):
-        dx = -4 * cell.fate.x**3 + 8 * cell.fate.x * cell.fate.y
-        dy = -4 * cell.fate.y**3 + 3 * cell.fate.y**2 + 4 * cell.fate.x**2 - 2 * self.K1 * cell.fate.y
+    def gradient(self, cell: Fate):
+        dx = -4 * cell.x ** 3 + 8 * cell.x * cell.y + self.f_1
+        dy = -4 * cell.y ** 3 + 3 * cell.y ** 2 + 4 * cell.x ** 2 - 2 * self.K1 * cell.y
         return dx, dy
 
+
 @dataclass
-class HeteroclinicFlipModel(GeometricalModel):
+class HeteroclinicFlip(GeomModel): # Heterochronic Flip model from Raju & Siggia (2024)
     K2: float = 1.5
+    fs: float = 0.1
+    b: float = -0.004
+    fex: float = 0.2  # or -0.3 for the inhibitor
 
-    def potential(self, cell: Cell):
-	return cell.fate.x**4 + cell.fate.y**4 - cell.fate.y**3 + 2 * cell.fate.x**2 * cell.fate.y - cell.fate.y**2 + self.K2 * cell.fate.y
+    def calculate_f2(self, population: list[Cell]):
+        positive_x_sum = sum(cell.fate.x for cell in population if cell.fate.x > 0)
+	positive_x_sum /= len(population)
+        return self.fs * positive_x_sum + self.b + self.fex
 
-    def gradient(self, cell: Cell):
-        dx = -4 * cell.fate.x**3 + 4 * cell.fate.x * cell.fate.y
-        dy = -4 * cell.fate.y**3 + 3 * cell.fate.y**2 + 2 * cell.fate.x**2 + 2 * self.K2 * cell.fate.y
+    def potential(self, cell: Fate, population: list[Cell]):
+	f2 = self.calculate_f2(population)
+	return cell.x ** 4 + cell.y ** 4 - cell.y ** 3 + 2 * cell.x ** 2 * cell.y - cell.y ** 2 + f2 * cell.x + self.K2 * cell.y
+
+    def gradient(self, cell: Fate):
+	f2 = self.calculate_f2(population)
+        dx = -4 * cell.x ** 3 + 4 * cell.x * cell.y + f2
+        dy = -4 * cell.y ** 3 + 3 * cell.y ** 2 + 2 * cell.x ** 2 + 2 * self.K2 * cell.y
         return dx, dy
