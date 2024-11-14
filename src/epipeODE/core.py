2688bebafb53a9c063ef109aed1eef5e7920e408
Esteban Nocet-Binois <en4624@princeton.edu>
2024-11-14 12:04:07 -0500

    Noxfile + update on precommit + new CI

--- src/epipeODE/core.py
+++ src/epipeODE/core.py
@@ -9,7 +9,7 @@ class Point:  # holds the position of a point in a 3D sapce (unused dimensions a
     y: int = 0
     z: int = 0
 
-    def dist(self, dest):
+    def dist(self, dest: "Point") -> float:
         return (
             (self.x - dest.x) ** 2 + (self.y - dest.y) ** 2 + (self.z - dest.z) ** 2
         ) ** (1 / 2)
@@ -17,11 +17,11 @@ class Point:  # holds the position of a point in a 3D sapce (unused dimensions a
 
 @dataclasses.dataclass
 class Fate:  # holds the ODE x, y (EPI-PE axis, Specification axis)
-    def __init__(self, x, y):
+    noise_level: float = 0.05
+
+    def __init__(self, x: float, y: float):
         self.x = x
         self.y = y
-
-    noise_level: float = 0.05
 
     def apply_noise(self):
         self.x += np.random.normal(0, self.noise_level)
