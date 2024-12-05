c8e75c0595fb5d4f94ac3bd4aadbcc01a2db5750
Huxley Marvit <huxley@princeton.edu>
2024-12-04 20:36:16 -0800

    init graphing

--- src/epipeODE/__init__.py
+++ src/epipeODE/__init__.py
@@ -1,20 +1,27 @@
-import dataclasses
+from dataclasses import dataclass
 from abc import ABC, abstractmethod
 import numpy as np
+from numpy.typing import NDArray
 
-@dataclasses.dataclass
+@dataclass
 class Point: # holds the position of a point in a 3D space (unused dimensions are set to 0)
-    x: int = 0
-    y: int = 0
-    z: int = 0
+    x: float = 0
+    y: float = 0
+    z: float = 0
     def dist(self, dest):
         return ((self.x - dest.x) ** 2 + (self.y - dest.y) ** 2 + (self.z - dest.z) ** 2) ** (1/2)
 
-@dataclasses.dataclass
+    def __getitem__(self, key: int) -> float:
+        return [self.x, self.y, self.z][key]
+
+    def to_tuple(self) -> NDArray[np.float64]:
+        return np.array([self.x, self.y, self.z])
+
+@dataclass
 class Fate: # holds the ODE x, y (EPI-PE axis, Specification axis)
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
+        self.x: float = x
+        self.y: float = y
 
     noise_level: float = 0.05
 
@@ -22,19 +29,22 @@ class Fate: # holds the ODE x, y (EPI-PE axis, Specification axis)
         self.x += np.random.normal(0, self.noise_level)
         self.y += np.random.normal(0, self.noise_level)
 
-@dataclasses.dataclass
+@dataclass
 class Cell: # object of a single cell specifying location and fate
     def __init__(self, p: Point, f: Fate):
         self.loc = p
         self.fate = f
         self.history = [[p, f]]
+    def __repr__(self):
+        return f"Cell(x={self.loc.x:.2f}, y={self.loc.y:.2f}, z={self.loc.z:.2f}, fx={self.fate.x:.2f}, fy={self.fate.x:.2f})"
 
-@dataclasses.dataclass
+# GEOM MODELS #
+@dataclass
 class GeomModel(ABC): # general modeling class
     dt: float = 0.01
 
     @abstractmethod
-    def potential(self, cell: Fate):
+    def potential(self, cell: Fate) -> float:
         pass
 
     @abstractmethod
@@ -54,7 +64,7 @@ class GeomModel(ABC): # general modeling class
 
 """
 
-@dataclasses.dataclass
+@dataclass
 class DualCusp(GeomModel): # Dual Cusp model from Raju & Siggia (2024)
     K1: float = 0.15
 
@@ -66,8 +76,7 @@ class DualCusp(GeomModel): # Dual Cusp model from Raju & Siggia (2024)
         dy = -4 * cell.y ** 3 + 3 * cell.y ** 2 + 4 * cell.x ** 2 - 2 * self.K1 * cell.y
         return dx, dy
 
-
-@dataclasses.dataclass
+@dataclass
 class HeteroclinicFlip(GeomModel): # Heterochronic Flip model from Raju & Siggia (2024)
     K2: float = 1.5
 
@@ -78,3 +87,44 @@ class HeteroclinicFlip(GeomModel): # Heterochronic Flip model from Raju & Siggia
         dx = -4 * cell.x ** 3 + 4 * cell.x * cell.y
         dy = -4 * cell.y ** 3 + 3 * cell.y ** 2 + 2 * cell.x ** 2 + 2 * self.K2 * cell.y
         return dx, dy
+
+@dataclass
+class Embryo:
+    def __init__(self, model: GeomModel, cells: list[Cell]):
+        self.model = model
+        self.cells = cells
+
+    def __repr__(self):
+        return f"Embryo(model={self.model}, #cells={len(self.cells)})"
+
+@dataclass
+class History:
+    def __init__(self):
+        self.history: list[Embryo] = []
+
+    def add(self, embryo: Embryo) -> None:
+        self.history.append(embryo)
+
+
+##### TESTING SETUP #####
+
+def initialize_embryo(model: GeomModel, num_cells: int) -> Embryo:
+    cells: list[Cell] = []
+    bounds = (-0.3, 0.3)
+    for _ in range(num_cells):
+        x, y = np.random.uniform(*bounds), np.random.uniform(*bounds)
+        cells.append(
+                Cell(
+                    p=Point(
+                        x, y, model.potential(Fate(x, y))
+                    ),
+                    f=Fate( # TODO idk what this should be
+                        np.random.uniform(*bounds),
+                        np.random.uniform(*bounds)
+                    )
+                )
+            )
+
+    return Embryo(model, cells)
+
+
--- src/epipeODE/visualization.py
+++ src/epipeODE/visualization.py
@@ -1,8 +1,105 @@
 import numpy as np
 from rich import print
 import matplotlib.pyplot as plt
+from matplotlib import cbook, cm
+from matplotlib.colors import LightSource
 
 from __init__ import * #TODO fixme? idk if these should be here
+
+
+class Visualizer:
+    def __init__(self):
+        pass
+
+    def plot_landscape(self, embryo: Embryo, show: bool = True) -> None:
+        """
+        Creates a 3d plot of the geometrical model landscape.
+        :param embryo: the Embryo to visualize
+        :param show: whether to display the plot
+        :return: None
+        """
+
+        SCALE = 2 # how  much bigger the plot should be than the point bounding box
+        GRANULARITY = 100 # how many points to sample in each dimension
+        # GROUND_OFFSET = 1 # how high to place off the ground
+        MIN_BOUND = 1
+
+        # get bounds for the plot
+        embryo_coords = np.array([cell.loc.to_tuple() for cell in embryo.cells])
+        min_x, max_x = np.min(embryo_coords[:, 0]), np.max(embryo_coords[:, 0])
+        min_y, max_y = np.min(embryo_coords[:, 1]), np.max(embryo_coords[:, 1])
+
+        # make sure we have a minimum bound
+        min_x = min(min_x, -MIN_BOUND/SCALE)
+        max_x = max(max_x, MIN_BOUND/SCALE)
+        min_y = min(min_y, -MIN_BOUND/SCALE)
+        max_y = max(max_y, MIN_BOUND/SCALE)
+
+        # create our meshgrid
+        X = np.linspace(min_x*SCALE, max_x*SCALE, GRANULARITY)
+        Y = np.linspace(min_y*SCALE, max_y*SCALE, GRANULARITY)
+        X, Y = np.meshgrid(X, Y)
+        Z = embryo.model.potential(Fate(X, Y))
+
+        min_z, max_z = np.min(Z), np.max(Z)
+
+        # let's plot!
+        fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+
+        # shading
+        ls = LightSource(0, 0)
+        rgb = ls.shade(Z, cmap=cm.viridis, vert_exag=0.1, blend_mode='soft')
+
+        # plot our surface
+        ax.plot_surface(X, Y, Z,
+                        cmap='viridis',
+                        facecolors=rgb, # with our cutstom shading
+                        linewidth=0,
+                        antialiased=False,
+                        shade=False,
+                        alpha=0.8,
+                        rstride=5,
+                        cstride=5,
+                        # edgecolor='black',
+                        # lw=0.01,
+                    )
+
+        # contour plot
+        ax.contourf(
+                X, Y, Z,
+                zdir='z',
+                offset=-max_z,
+                cmap='RdYlBu_r',
+                # linewidths=0.5,
+                # linestyle='solid',
+                )
+
+        # plot our cells
+        for cell in embryo.cells:
+            ax.scatter(cell.loc.x, cell.loc.y, cell.loc.z, color='red')
+
+        # plot styling
+        ax.grid(False)
+        ax.xaxis.pane.fill = False
+        ax.yaxis.pane.fill = False
+        ax.zaxis.pane.fill = False
+        ax.set(zlim=-max_z)
+        # ax.view_init(elev=20, azim=45)
+        if show: plt.show()
+
+
+        # raise NotImplementedError
+
+    def plot_trajectories(self, history: History, show: bool = True) -> None:
+        """
+        Creates a 2d plot of the trajectories of the cells in the history.
+        :param history: the History to visualize
+        :param show: whether to display the plot
+        :return: None
+        """
+
+        raise NotImplementedError
+
 
 
 def very_temp_plot_surface():
@@ -30,12 +127,15 @@ def very_temp_plot_surface():
     plt.show()
 
 
-
-
 if __name__ == '__main__':
     geom_model = HeteroclinicFlip()
-    print("geom_model", geom_model)
+    # print("geom_model", geom_model)
 
-    very_temp_plot_surface()
+    # very_temp_plot_surface()
 
+    embryo = initialize_embryo(geom_model, 100)
+    print("embryo", embryo)
+
+    v = Visualizer()
+    v.plot_landscape(embryo)
 
