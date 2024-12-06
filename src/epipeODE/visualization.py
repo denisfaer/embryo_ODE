e30099a667515ead9d612e88a8a4ca1214d7f072
Huxley Marvit <huxley@princeton.edu>
2024-12-05 18:52:34 -0800

    mild cleaning

--- src/epipeODE/visualization.py
+++ src/epipeODE/visualization.py
@@ -172,43 +172,12 @@ class Visualizer:
 
         if show: plt.show()
 
-
-def very_temp_plot_surface():
-    fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
-    X = np.arange(-10, 10, 0.25)
-    Y = np.arange(-10, 10, 0.25)
-    X, Y = np.meshgrid(X, Y)
-    Z = geom_model.potential(Fate(X, Y))
-
-
-    ax.plot_surface(X, Y, Z+np.max(Z)*0.5, cmap='viridis')
-
-    zmin = np.min(Z)
-    ax.contourf(X, Y, Z, 20, cmap='RdYlBu_r', offset=zmin)
-
-    ax.grid(False)
-    ax.xaxis.pane.fill = False
-    ax.yaxis.pane.fill = False
-    ax.zaxis.pane.fill = False
-    ax.view_init(elev=20, azim=45)
-
-
-    plt.show()
-
-
 if __name__ == '__main__':
     geom_model = HeteroclinicFlip()
-    # print("geom_model", geom_model)
-
-    # very_temp_plot_surface()
-
     embryo = initialize_embryo(geom_model, 100)
-    # print("embryo", embryo)
 
     v = Visualizer()
-    # v.plot_landscape(embryo)
+    v.plot_landscape(embryo)
 
     hist = generate_fake_history(embryo, 100)
-    print("hist", hist)
     v.plot_trajectories(hist)
-
