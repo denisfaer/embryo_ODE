015dc9a9136c4ed5f4ffc872cd91ec3235c4013d
michel-mata <sm75@princeton.edu>
2024-12-10 15:12:06 -0500

    fix: import GeomModel in data_classes

--- src/epipeODE/data_classes.py
+++ src/epipeODE/data_classes.py
@@ -1,7 +1,9 @@
+from __future__ import annotations
 from dataclasses import dataclass
 from typing import List, Iterator
 import numpy as np
 from numpy.typing import NDArray
+from .models import GeomModel
 
 @dataclass
 class Point:
